0004abbaafffab266d555a2bc3fd81dc9c9c7e8a
Rafael <rafcasto@gmail.com>
2020-08-06 00:43:55 +1200

    adding sample code

---
+++ NUnit_LoginTest/Features/Login.feature
@@ -0,0 +1,23 @@
+﻿Feature: Login
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Add two numbers
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+
+	Scenario Outline: Add two numbers multiple
+	Given the first number is <firstNumber>
+	And the second number is <secondNumber>
+	When the two numbers are added
+	Then the result should be <result>
+	Examples: 
+	| firstNumber | secondNumber | result |
+	| 1           | 2            | 3      |
+	| 2           | 20            | 30      |
+	| 1           | 20            | 37      |
+	| 1           | 24           | 3      |
---
+++ NUnit_LoginTest/StepDefs/LoginSteps.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace NUnit_LoginTest.StepDefs
+{
+    [Binding]
+    public class LoginSteps
+    {
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            Assert.Fail();
+         }
+        
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
